40920fe4b1a95bca98136e9f5778149f5cc60428
Carly Pennock <carly.pennock@gmail.com>
2020-09-09 21:06:32 -0500

    Updates

---
+++ precache-manifest.d48db08bb19e4db24495617f20bcfd63.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f50a28f7ce84447369ccb4d6dba166",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1df3dc54c37097dfef2",
+    "url": "./static/css/main.bffc318f.chunk.css"
+  },
+  {
+    "revision": "8999e8a889b1fa4faaee",
+    "url": "./static/js/2.106376b9.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "./static/js/2.106376b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1df3dc54c37097dfef2",
+    "url": "./static/js/main.ed1cecb8.chunk.js"
+  },
+  {
+    "revision": "4a53e20cf63b066f46e3",
+    "url": "./static/js/runtime-main.6ff86fd8.js"
+  },
+  {
+    "revision": "6b0d2c56f733c6cb933c5afc80c00c28",
+    "url": "./static/media/landing.6b0d2c56.jpeg"
+  },
+  {
+    "revision": "6aa876a6e8806259440f13f5608c59a3",
+    "url": "./static/media/portrait.6aa876a6.jpg"
+  }
+]);
